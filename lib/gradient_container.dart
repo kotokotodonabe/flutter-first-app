e2cf4820749aa09103c62a8fb8b652780e562ea9
kotokotodonabe <ksk.pleiades@gmail.com>
2024-07-20 15:35:06 +0900

    fix：gradientのAlignmentを変数にした

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:first_app/styled_text.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
@@ -10,11 +13,11 @@ class GradientContainer extends StatelessWidget {
       decoration: const BoxDecoration(
         gradient: LinearGradient(
           colors: [
-            Color.fromARGB(255, 67, 8, 168),
+            Color.fromARGB(255, 59, 6, 150),
             Color.fromARGB(255, 61, 1, 163)
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
