12c6ff4e984405f1929af738591570704fd57735
kotokotodonabe <ksk.pleiades@gmail.com>
2024-07-20 14:22:00 +0900

    feature：Container WidgetをClassに切り分けた

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,40 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 237, 235, 243),
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 67, 8, 168),
+            Color.fromARGB(255, 61, 1, 163)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            color: Color(0xFF42A5F5),
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
 }
