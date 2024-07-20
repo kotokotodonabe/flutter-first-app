0ef5f94b2c7e94a181c6f10c371997533c611c4a
kotokotodonabe <ksk.pleiades@gmail.com>
2024-07-20 15:07:16 +0900

    fix：テキストスタイルをクラスに切り分け

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -17,13 +18,7 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: const Center(
-        child: Text(
-          'Hello World!',
-          style: TextStyle(
-            color: Color.fromARGB(255, 6, 4, 23),
-            fontSize: 28,
-          ),
-        ),
+        child: StyledText(),
       ),
     );
   }
