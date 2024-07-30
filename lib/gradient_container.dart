4946afefd7ce572394e3bde28c4e503e4121a2d2
kotokotodonabe <ksk.pleiades@gmail.com>
2024-07-30 23:24:26 +0900

    途中だが一旦コミット

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,18 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/dice_roller.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  GradientContainer(this.firstColor, this.secondColor, {super.key});
+  const GradientContainer(this.firstColor, this.secondColor, {super.key});
 
   final Color firstColor;
   final Color secondColor;
-  var activeDiceImage = 'assets/images/dice-2.png';
-
-  void rollDice() {
-    activeDiceImage = 'assets/images/dice-4.png';
-  }
 
   @override
   Widget build(context) {
@@ -28,28 +24,7 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              activeDiceImage,
-              width: 200,
-            ),
-            TextButton(
-              onPressed: () {},
-              style: TextButton.styleFrom(
-                padding: const EdgeInsets.only(
-                  top: 20,
-                ),
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(
-                  fontSize: 28,
-                ),
-              ),
-              child: const Text('Roll dice'),
-            ),
-          ],
-        ),
+        child: DiceRoller(),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,11 @@ import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         body: GradientContainer(
-          const Color.fromARGB(255, 59, 6, 150),
-          const Color.fromARGB(255, 61, 1, 163),
+          Color.fromARGB(255, 59, 6, 150),
+          Color.fromARGB(255, 61, 1, 163),
         ),
       ),
     ),
